a80f1c85129d42b8692a424e15a4b1042947a4ca
roland-bsc <rabellaatbsc@gmail.com>
2021-11-30 22:11:17 -0800

    added new stuff

--- Week5/week5_9.4.py
+++ Week5/week5_9.4.py
@@ -16,6 +16,8 @@ for line in handle:
     else:
         email_counts[email] += 1
 
+#print(email_counts)
+
 max_key = max(email_counts, key=email_counts.get) #find the key with the highest value using max() method
 
 print(max_key, email_counts.get(max_key,0)) #print(key with highest value, get the value of the key using get() method
